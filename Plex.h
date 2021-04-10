b284136ce7ab39693f6786859e40ee581a94900e
Maria Barysheva <71269683+MBarysheva@users.noreply.github.com>
2021-04-10 15:23:08 +0300

    Plex with show

--- Plex.h
+++ Plex.h
@@ -31,9 +31,56 @@ public:
 		st.Push(CurrLine);
 		while (!st.Empty())
 		{
-			CurrLine
+			CurrLine = st.Pop();
+			while (CurrLine.pFp == NULL)
+			{
+				pr = CurrLine.pChart->GetFirst();
+				pp = dynamic_cast<TPoint*>(pr);
+				if (pp != NULL)
+				{
+					CurrLine.pFp = pp;
+					pp->Show(gr);
+				}
+				else
+				{
+					st.Push(CurrLine);
+				    CurrLine.pChart = dynamic_cast<TChart*>(pr);
+				}
+			}
+			if (CurrLine.pLp == NULL)
+			{
+				pr = CurrLine.pChart->GetLast();
+				pp = dynamic_cast<TPoint*>(pr);
+				if (pp != NULL)
+				{
+					CurrLine.pLp = pp;
+					pp->Show(gr);
+				}
+				else
+				{
+					st.Push(CurrLine);
+					CurrLine.pChart = dynamic_cast<TChart*>(pr);
+					CurrLine.pFp = NULL;
+					st.Push(CurrLine);
+				}
+			}
+			if (CurrLine.pFp != NULL && CurrLine.pLp != NULL)
+			{
+				gr->DrawLine(Pens::Black, CurrLine.pFp = GetX(), CurrLine.pFp = GetY(), CurrLine.pLp = GetX(), CurrLine.pLp = GetY());
+				pp = CurrLine.pLp;
+				if (!st.Empty())
+				{
+					CurrLine = st.Pop();
+					if (CurrLine.pFp == NULL)
+					{
+						CurrLine.pLp = pp;
+
+					}
+				}
+				st.Push(CurrLine);
+			}
 		}
 
 	}
 };
-}
+
